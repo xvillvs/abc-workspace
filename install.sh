d5d10e91cfb51a7df5be48a6fef0882d080ad480
kimerran <mh.neri@gmail.com>
2024-01-26 21:26:49 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 # Install Rust and Cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
+source "$HOME/.cargo/env"
+
 # Install cargo-stylus
 RUSTFLAGS="-C link-args=-rdynamic" cargo install --force cargo-stylus
 rustup target add wasm32-unknown-unknown
+
+# Test cargo-stylus
+# cargo stylus --help
